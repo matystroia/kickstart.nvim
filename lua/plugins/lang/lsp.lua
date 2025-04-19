f52cf242fd7b71af8b806425c5cec00dd371c087
Matei Stroia <stroiamatei@gmail.com>
2025-04-19 04:01:40 +0300

    add explicit lsp and formatters to mason

--- lua/plugins/lang/lsp.lua
+++ lua/plugins/lang/lsp.lua
@@ -133,9 +133,6 @@ return {
         --
 
         lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
           settings = {
             Lua = {
               completion = {
@@ -145,18 +142,13 @@ return {
             },
           },
         },
-        basedpyright = {
-          settings = {},
-        },
       }
 
-      -- Ensure the servers and tools above are installed
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-      })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+      local ensure_installed = {}
+      vim.list_extend(ensure_installed, { 'lua_ls', 'basedpyright', 'bashls', 'cssls', 'hyprls' })
+      vim.list_extend(ensure_installed, { 'stylua', 'isort', 'black', 'prettierd', 'shfmt' })
 
+      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
       require('mason-lspconfig').setup {
         ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
         automatic_installation = false,
