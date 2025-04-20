71449c314c1b48641e3513eff975cd1f4ea22a8b
Matei Stroia <stroiamatei@gmail.com>
2025-04-20 17:34:23 +0300

    disable copilot in comments

--- lua/plugins/extra/copilot.lua
+++ lua/plugins/extra/copilot.lua
@@ -8,6 +8,20 @@ return {
         replace_keycodes = false,
       })
       vim.g.copilot_no_tab_map = true
+
+      -- FIXME: Should use treesitter context when available
+      vim.api.nvim_create_autocmd('InsertEnter', {
+        group = vim.api.nvim_create_augroup('copilot-comments', { clear = true }),
+        desc = 'Smecheri fut fraeri comenteaza nene cu multa stima si respect',
+        callback = function()
+          if vim.bo.commentstring == '' then
+            return
+          end
+          -- Remove `%s` and escape all punctuation
+          local pattern = '^%s*' .. vim.bo.commentstring:gsub('%%s', ''):gsub('(%p)', '%%%0')
+          vim.b.copilot_enabled = not vim.api.nvim_get_current_line():match(pattern)
+        end,
+      })
     end,
   },
   {
