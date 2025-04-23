77b96bd8a623ddb37afef23b29cf4b1a1d504a29
Matei Stroia <stroiamatei@gmail.com>
2025-04-24 00:49:33 +0300

    remove mini.ai

--- lua/plugins/basic/operator.lua
+++ lua/plugins/basic/operator.lua
@@ -4,10 +4,6 @@ return {
   'tpope/vim-surround',
   -- TODO: Look at all mini.nvim plugins
   {
-    'echasnovski/mini.ai',
-    opts = { n_lines = 500 },
-  },
-  {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
     opts = {},
