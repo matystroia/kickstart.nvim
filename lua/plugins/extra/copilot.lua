520c7ac772ac80472e2aac2847602310c68510a2
Matei Stroia <stroiamatei@gmail.com>
2025-04-24 01:21:58 +0300

    replace POS copilot plugin :) (provided by a billion dollar company btw)

--- lua/plugins/extra/copilot.lua
+++ lua/plugins/extra/copilot.lua
@@ -1,13 +1,49 @@
 ---@type (LazyPluginSpec | string)[]
 return {
   {
-    'github/copilot.vim',
+    'zbirenbaum/copilot.lua',
+    event = 'InsertEnter',
     config = function()
-      vim.keymap.set('i', '<C-y>', 'copilot#Accept("")', {
-        expr = true,
-        replace_keycodes = false,
+      local copilot = require 'copilot'
+      local suggestion = require 'copilot.suggestion'
+
+      copilot.setup {
+        panel = { enabled = false },
+        suggestion = {
+          auto_trigger = false,
+          keymap = {
+            accept = '<C-l>',
+          },
+        },
+        copilot_model = 'gpt-4o-copilot',
+      }
+
+      vim.keymap.set('n', '<Leader>tc', function()
+        if vim.b.copilot_suggestion_auto_trigger then
+          suggestion.toggle_auto_trigger()
+          vim.notify('[Copilot] Disabled :)', vim.log.levels.INFO)
+        else
+          suggestion.toggle_auto_trigger()
+          vim.notify('[Copilot] Enabled', vim.log.levels.INFO)
+        end
+      end, { desc = 'Toggle Copilot' })
+
+      -- Disable copilot suggestions while autocomplete menu is visible
+      local blink_group = vim.api.nvim_create_augroup('copilot-disable-blink', { clear = true })
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'BlinkCmpMenuOpen',
+        group = blink_group,
+        callback = function()
+          vim.b.copilot_suggestion_hidden = true
+        end,
       })
-      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'BlinkCmpMenuClose',
+        group = blink_group,
+        callback = function()
+          vim.b.copilot_suggestion_hidden = false
+        end,
+      })
 
       local timer, err = vim.uv.new_timer()
       if timer == nil then
@@ -18,43 +54,36 @@ return {
       local function check_context()
         local row, col = unpack(vim.api.nvim_win_get_cursor(0))
         row, col = row - 1, math.max(0, col - 1) -- Just left of cursor
-        for _, capture in ipairs(vim.treesitter.get_captures_at_pos(0, row, col)) do
-          if capture.capture == 'comment' then
-            vim.b.copilot_enabled = false
-            return
-          end
-        end
-        vim.b.copilot_enabled = nil
+        local is_comment = vim.iter(vim.treesitter.get_captures_at_pos(0, row, col)):any(function(cap)
+          return cap.capture == 'comment'
+        end)
+        vim.b.copilot_suggestion_hidden = is_comment
       end
 
-      local group = vim.api.nvim_create_augroup('copilot-shutup', { clear = true })
+      -- Disable copilot suggestions in comments
+      local comment_group = vim.api.nvim_create_augroup('copilot-disable-comments', { clear = true })
       vim.api.nvim_create_autocmd('BufEnter', {
-        group = group,
+        group = comment_group,
         callback = function()
-          if vim.b.has_treesitter ~= nil then
-            return
-          end
-          local is_file_buf = vim.bo.filetype ~= '' and vim.bo.buftype == ''
-          vim.b.has_treesitter = is_file_buf and vim.treesitter.get_parser(nil, nil, { error = false }) ~= nil
+          vim.b.has_treesitter = vim.bo.filetype ~= '' and vim.treesitter.get_parser(nil, nil, { error = false }) ~= nil
         end,
       })
       vim.api.nvim_create_autocmd('InsertEnter', {
-        group = group,
+        group = comment_group,
         callback = function()
-          if not vim.b.has_treesitter then
-            return
+          if vim.b.has_treesitter then
+            timer:start(0, 500, vim.schedule_wrap(check_context))
           end
-          timer:start(0, 500, vim.schedule_wrap(check_context))
         end,
       })
       vim.api.nvim_create_autocmd({ 'BufLeave', 'InsertLeave' }, {
-        group = group,
+        group = comment_group,
         callback = function()
           timer:stop()
         end,
       })
       vim.api.nvim_create_autocmd('VimLeavePre', {
-        group = group,
+        group = comment_group,
         callback = function()
           timer:close()
         end,
