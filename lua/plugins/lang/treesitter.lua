4fcaeb878046550f2e4e7d946421c1d54e90d008
Matei Stroia <stroiamatei@gmail.com>
2025-05-11 18:42:34 +0300

    treesitter-textobjects

--- lua/plugins/lang/treesitter.lua
+++ lua/plugins/lang/treesitter.lua
@@ -23,6 +23,13 @@ return {
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true, disable = { 'ruby' } },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          node_incremental = '>',
+          node_decremental = '<',
+        },
+      },
     },
     init = function()
       vim.filetype.add {
@@ -31,17 +38,92 @@ return {
         },
       }
     end,
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
   {
-    'nvim-treesitter/nvim-treesitter-context',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-    },
-    opts = { mode = 'cursor', max_lines = 2, trim_scope = 'outer' },
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'jinh0/eyeliner.nvim', 'ggandor/leap.nvim' },
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ['if'] = '@function.inner',
+              ['af'] = '@function.outer',
+
+              ['iC'] = '@class.inner',
+              ['aC'] = '@class.outer',
+
+              ['as'] = '@statement.outer',
+
+              ['ic'] = '@call.inner',
+              ['ac'] = '@call.outer',
+
+              ['al'] = '@assignment.lhs',
+              ['ar'] = '@assignment.rhs',
+
+              ['ia'] = '@parameter.inner',
+              ['aa'] = '@parameter.outer',
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = false,
+            goto_next_start = {
+              [']f'] = '@function.outer',
+              [']s'] = '@statement.outer',
+              [']p'] = '@parameter.inner',
+              [']C'] = '@class.outer',
+            },
+            goto_next_end = {
+              [']F'] = '@function.outer',
+              [']S'] = '@statement.outer',
+            },
+            goto_previous_start = {
+              ['[f'] = '@function.outer',
+              ['[s'] = '@statement.outer',
+              ['[p'] = '@parameter.inner',
+              ['[C'] = '@class.outer',
+            },
+            goto_previous_end = {
+              ['[F'] = '@function.outer',
+              ['[S'] = '@statement.outer',
+            },
+          },
+        },
+      }
+      -- TODO: Move all of this to somewhere more generic, create own repeat_move
+      local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
+      local eyeliner = require 'eyeliner'
+      local leap = require 'leap'
+
+      vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move)
+      vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_opposite)
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'LeapLeave',
+        callback = function()
+          local forward = leap.state['repeat'].backward ~= true
+          ts_repeat_move.set_last_move(function(opts)
+            leap.leap { ['repeat'] = true, backward = not opts.forward }
+          end, { forward = forward })
+        end,
+      })
+
+      vim.iter({ 'f', 'F', 't', 'T' }):each(function(op)
+        local forward = op ~= op:upper()
+        vim.keymap.set({ 'n', 'x', 'o' }, op, function()
+          eyeliner.highlight { forward = forward }
+          ts_repeat_move.last_move = {
+            func = op,
+            opts = { forward = forward },
+            additional_args = {},
+          }
+          return op
+        end, { expr = true })
+      end)
+    end,
   },
   {
     'elkowar/yuck.vim',
