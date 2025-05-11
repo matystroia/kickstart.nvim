6e573ad422ec0ebe53a2e9f19282e9033c2d0430
Matei Stroia <stroiamatei@gmail.com>
2025-05-11 19:36:15 +0300

    move blink to sep file

--- lua/plugins/lang/lsp.lua
+++ lua/plugins/lang/lsp.lua
@@ -5,8 +5,8 @@ return {
     'neovim/nvim-lspconfig',
     dependencies = {
       { 'mason-org/mason.nvim', opts = {} },
-      'mason-org/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
+      { 'mason-org/mason-lspconfig.nvim', opts = {} },
+      { 'WhoIsSethDaniel/mason-tool-installer.nvim', opts = {} },
 
       'saghen/blink.cmp',
     },
@@ -142,85 +142,6 @@ return {
         automatic_enable = true,
       }
     end,
-  },
-  {
-    'saghen/blink.cmp',
-    event = 'InsertEnter',
-    build = 'cargo build --release',
-    dependencies = {
-      {
-        'L3MON4D3/LuaSnip',
-        version = '2.*',
-        build = 'make install_jsregexp',
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
-        },
-        opts = {
-          region_check_events = { 'InsertEnter' },
-        },
-      },
-      'folke/lazydev.nvim',
-    },
-    --- @module 'blink.cmp'
-    --- @type blink.cmp.Config
-    opts = {
-      keymap = {
-        -- For an understanding of why the 'default' preset is recommended,
-        -- you will need to read `:help ins-completion`
-        --
-        -- No, but seriously. Please read `:help ins-completion`, it is really good!
-        --
-        -- All presets have the following mappings:
-        -- <tab>/<s-tab>: move to right/left of your snippet expansion
-        -- <c-space>: Open menu or open docs if already open
-        -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-        -- <c-e>: Hide menu
-        -- <c-k>: Toggle signature help
-        preset = 'default',
-
-        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-      },
-      appearance = {
-        nerd_font_variant = 'normal',
-      },
-      completion = {
-        menu = {
-          border = 'none',
-        },
-        list = {
-          selection = {
-            auto_insert = false,
-          },
-        },
-        documentation = {
-          auto_show = false,
-          window = { border = 'rounded' },
-        },
-      },
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'lazydev' },
-        providers = {
-          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
-        },
-      },
-      snippets = { preset = 'luasnip' },
-      fuzzy = { implementation = 'rust' },
-      signature = {
-        enabled = true,
-        window = {
-          show_documentation = false,
-        },
-      },
-    },
   },
   {
     'folke/lazydev.nvim',
