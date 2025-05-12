e1f49f32cafe057c899b7e75f0381567a34a0bd8
Matei Stroia <stroiamatei@gmail.com>
2025-05-12 12:31:05 +0300

    minor

--- lua/plugins/basic/motion.lua
+++ lua/plugins/basic/motion.lua
@@ -13,11 +13,16 @@ return {
   },
   {
     'ggandor/leap.nvim',
+    lazy = false,
+    dependencies = { 'tpope/vim-repeat' },
     config = function()
       local leap = require 'leap'
       leap.opts.case_sensitive = false
+
       vim.keymap.set('n', 's', '<Plug>(leap-forward)')
       vim.keymap.set('n', 'S', '<Plug>(leap-backward)')
+
+      vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Whitespace' })
     end,
   },
 }
