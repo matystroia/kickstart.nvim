7b78299bde5937f820df6e50aba09123d6b7685d
Matei Stroia <stroiamatei@gmail.com>
2025-09-07 17:49:16 +0300

    remove unnecessary capabilities config

--- lua/plugins/lsp/blink.lua
+++ lua/plugins/lsp/blink.lua
@@ -1,7 +1,7 @@
 return {
   {
     'saghen/blink.cmp',
-    event = 'InsertEnter',
+    event = 'VeryLazy',
     build = 'cargo build --release',
     dependencies = {
       {
@@ -25,63 +25,60 @@ return {
       },
       'folke/lazydev.nvim',
     },
-    config = function()
-      local blink = require 'blink.cmp'
-      blink.setup {
-        keymap = {
-          -- For an understanding of why the 'default' preset is recommended,
-          -- you will need to read `:help ins-completion`
-          --
-          -- No, but seriously. Please read `:help ins-completion`, it is really good!
-          --
-          -- All presets have the following mappings:
-          -- <tab>/<s-tab>: move to right/left of your snippet expansion
-          -- <c-space>: Open menu or open docs if already open
-          -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-          -- <c-e>: Hide menu
-          -- <c-k>: Toggle signature help
-          preset = 'default',
+    --- @module 'blink.cmp'
+    --- @type blink.cmp.Config
+    opts = {
+      keymap = {
+        -- For an understanding of why the 'default' preset is recommended,
+        -- you will need to read `:help ins-completion`
+        --
+        -- No, but seriously. Please read `:help ins-completion`, it is really good!
+        --
+        -- All presets have the following mappings:
+        -- <tab>/<s-tab>: move to right/left of your snippet expansion
+        -- <c-space>: Open menu or open docs if already open
+        -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
+        -- <c-e>: Hide menu
+        -- <c-k>: Toggle signature help
+        preset = 'default',
 
-          -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-          --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+      },
+      appearance = {
+        nerd_font_variant = 'normal',
+      },
+      completion = {
+        menu = {
+          border = 'none',
         },
-        appearance = {
-          nerd_font_variant = 'normal',
-        },
-        completion = {
-          menu = {
-            border = 'none',
-          },
-          list = {
-            selection = {
-              auto_insert = false,
-            },
-          },
-          documentation = {
-            auto_show = false,
-            window = { border = 'rounded' },
+        list = {
+          selection = {
+            auto_insert = false,
           },
         },
-        sources = {
-          default = { 'lsp', 'path', 'snippets', 'lazydev' },
-          providers = {
-            lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
-          },
+        documentation = {
+          auto_show = false,
+          window = { border = 'rounded' },
         },
-        snippets = { preset = 'luasnip' },
-        fuzzy = { implementation = 'rust' },
-        signature = {
-          enabled = true,
-          trigger = {
-            show_on_accept = true,
-          },
-          window = {
-            show_documentation = false,
-          },
+      },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'lazydev' },
+        providers = {
+          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
         },
-      }
-
-      vim.lsp.config('*', { capabilities = require('blink.cmp').get_lsp_capabilities() })
-    end,
+      },
+      snippets = { preset = 'luasnip' },
+      fuzzy = { implementation = 'rust' },
+      signature = {
+        enabled = true,
+        trigger = {
+          show_on_accept = true,
+        },
+        window = {
+          show_documentation = false,
+        },
+      },
+    },
   },
 }
