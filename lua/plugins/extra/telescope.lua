992673a59e6826b2b332ab3b184ee2e80d97c3e0
Matei Stroia <stroiamatei@gmail.com>
2026-05-11 14:29:55 +0300

    one plugin per file: telescope

--- lua/plugins/extra/telescope.lua
+++ lua/plugins/extra/telescope.lua
@@ -46,69 +46,67 @@ local function prefix_find_files(opts)
     :find()
 end
 
---- @type LazyPluginSpec[]
+--- @type LazyPluginSpec
 return {
-  {
-    'nvim-telescope/telescope.nvim',
-    event = 'VimEnter',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-      },
-      { 'nvim-telescope/telescope-ui-select.nvim' },
-      { 'nvim-tree/nvim-web-devicons' },
+  'nvim-telescope/telescope.nvim',
+  event = 'VimEnter',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
     },
-    config = function()
-      require('telescope').setup {
-        defaults = {
-          prompt_prefix = '',
-          selection_caret = ' ',
-          mappings = {
-            n = {
-              ['\\'] = require('telescope.actions').select_vertical,
-              ['|'] = require('telescope.actions').select_horizontal,
-              ['<C-v>'] = require('telescope.actions.layout').toggle_preview,
-            },
-            i = {
-              ['<C-v>'] = require('telescope.actions.layout').toggle_preview,
-            },
-          },
-        },
-        extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
-          },
-        },
-      }
-
-      require('telescope').load_extension 'fzf'
-      require('telescope').load_extension 'ui-select'
-
-      local builtin = require 'telescope.builtin'
-      local themes = require 'telescope.themes'
-
-      vim.keymap.set('n', '<Leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<Leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<Leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<Leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<Leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<Leader>so', builtin.oldfiles, { desc = '[S]earch [O]ld' })
-
-      vim.keymap.set('n', '<Leader>sp', function()
-        builtin.find_files { cwd = vim.fs.joinpath(vim.fn.stdpath 'data', 'lazy') }
-      end, { desc = '[S]earch [P]ackages' })
-
-      vim.keymap.set('n', '<Leader>s/', function()
-        builtin.current_buffer_fuzzy_find(themes.get_dropdown { previewer = false })
-      end, { desc = '[S]earch buffer' })
-
-      vim.keymap.set('n', '<Tab><Tab>', builtin.buffers, { desc = 'Find buffers' })
-
-      vim.keymap.set('n', '<C-p>', function()
-        prefix_find_files(themes.get_dropdown { previewer = false, prompt_title = 'Find' })
-      end)
-    end,
+    { 'nvim-telescope/telescope-ui-select.nvim' },
+    { 'nvim-tree/nvim-web-devicons' },
   },
+  config = function()
+    require('telescope').setup {
+      defaults = {
+        prompt_prefix = '',
+        selection_caret = ' ',
+        mappings = {
+          n = {
+            ['\\'] = require('telescope.actions').select_vertical,
+            ['|'] = require('telescope.actions').select_horizontal,
+            ['<C-v>'] = require('telescope.actions.layout').toggle_preview,
+          },
+          i = {
+            ['<C-v>'] = require('telescope.actions.layout').toggle_preview,
+          },
+        },
+      },
+      extensions = {
+        ['ui-select'] = {
+          require('telescope.themes').get_dropdown(),
+        },
+      },
+    }
+
+    require('telescope').load_extension 'fzf'
+    require('telescope').load_extension 'ui-select'
+
+    local builtin = require 'telescope.builtin'
+    local themes = require 'telescope.themes'
+
+    vim.keymap.set('n', '<Leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+    vim.keymap.set('n', '<Leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+    vim.keymap.set('n', '<Leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set('n', '<Leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+    vim.keymap.set('n', '<Leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+    vim.keymap.set('n', '<Leader>so', builtin.oldfiles, { desc = '[S]earch [O]ld' })
+
+    vim.keymap.set('n', '<Leader>sp', function()
+      builtin.find_files { cwd = vim.fs.joinpath(vim.fn.stdpath 'data', 'lazy') }
+    end, { desc = '[S]earch [P]ackages' })
+
+    vim.keymap.set('n', '<Leader>s/', function()
+      builtin.current_buffer_fuzzy_find(themes.get_dropdown { previewer = false })
+    end, { desc = '[S]earch buffer' })
+
+    vim.keymap.set('n', '<Tab><Tab>', builtin.buffers, { desc = 'Find buffers' })
+
+    vim.keymap.set('n', '<C-p>', function()
+      prefix_find_files(themes.get_dropdown { previewer = false, prompt_title = 'Find' })
+    end)
+  end,
 }
