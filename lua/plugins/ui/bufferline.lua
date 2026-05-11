36c8b844b78b82a9e3a89fdc36249328a1e6689b
Matei Stroia <stroiamatei@gmail.com>
2026-05-11 14:25:14 +0300

    one plugin per file: bufferline

--- lua/plugins/ui/bufferline.lua
+++ lua/plugins/ui/bufferline.lua
@@ -1,37 +1,35 @@
+--- @type LazyPluginSpec
 return {
-  {
-    'akinsho/bufferline.nvim',
-    config = function()
-      local bufferline = require 'bufferline'
-      bufferline.setup {
-        options = {
-          show_buffer_close_icons = false,
-          show_close_icon = false,
-          custom_areas = {
-            right = function()
-              local time = require('custom.wakatime').today()
-              if time == nil then
-                return { { text = '? ' } }
-              end
+  'akinsho/bufferline.nvim',
+  config = function()
+    require('bufferline').setup {
+      options = {
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        custom_areas = {
+          right = function()
+            local time = require('custom.wakatime').today()
+            if time == nil then
+              return { { text = '? ' } }
+            end
 
-              local ret = {}
-              if time.hours > 0 then
-                ret[#ret + 1] = string.format('%sh', time.hours)
-              end
-              if time.minutes > 0 then
-                ret[#ret + 1] = string.format('%sm', time.minutes)
-              end
+            local ret = {}
+            if time.hours > 0 then
+              ret[#ret + 1] = string.format('%sh', time.hours)
+            end
+            if time.minutes > 0 then
+              ret[#ret + 1] = string.format('%sm', time.minutes)
+            end
 
-              return { { text = table.concat(ret, ' ') .. ' ' } }
-            end,
-          },
+            return { { text = table.concat(ret, ' ') .. ' ' } }
+          end,
         },
-      }
+      },
+    }
 
-      vim.keymap.set('n', '<Tab>g', '<Cmd>BufferLinePick<CR>')
-      vim.keymap.set('n', '<Tab>D', '<Cmd>BufferLinePickClose<CR>')
-      vim.keymap.set('n', '<Tab>o', '<Cmd>BufferLineCloseOthers<CR>')
-    end,
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-  },
+    vim.keymap.set('n', '<Tab>g', '<Cmd>BufferLinePick<CR>')
+    vim.keymap.set('n', '<Tab>D', '<Cmd>BufferLinePickClose<CR>')
+    vim.keymap.set('n', '<Tab>o', '<Cmd>BufferLineCloseOthers<CR>')
+  end,
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
 }
