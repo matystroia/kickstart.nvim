b5d16c742b113f9daba4322ea5a3a519b6ca8dc9
Matei Stroia <stroiamatei@gmail.com>
2026-05-11 18:14:42 +0300

    one plugin per file: conform, guess-indent

--- lua/plugins/lang/conform.lua
+++ lua/plugins/lang/conform.lua
@@ -1,71 +1,73 @@
---- @type (LazyPluginSpec | string)[]
+--- @type LazyPluginSpec
 return {
-  { 'NMAC427/guess-indent.nvim', opts = {} },
-  {
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_format = 'fallback' }
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
     },
-    config = function()
-      -- TODO: Ensure installed
-      local formatters_by_ft = {
-        lua = { 'stylua' },
-        python = { 'ruff_format', 'isort' },
-        sh = { 'shfmt' },
-      }
-      local general_formatters = {
-        prettierd = { 'javascript', 'typescript', 'json', 'jsonc', 'html', 'javascriptreact', 'typescriptreact' },
-      }
-
-      formatters_by_ft = vim.tbl_extend(
-        'error',
-        formatters_by_ft,
-        vim.iter(pairs(general_formatters)):fold({}, function(acc, fmt, fts)
-          vim.iter(fts):each(function(ft)
-            acc[ft] = { fmt }
-          end)
-          return acc
-        end)
-      )
-
-      vim.keymap.set('n', '<Leader>tf', function()
-        if vim.b.conform_disabled then
-          vim.b.conform_disabled = false
-          vim.notify('[Conform] Enabled', vim.log.levels.INFO)
-        else
-          vim.b.conform_disabled = true
-          vim.notify('[Conform] Disabled', vim.log.levels.INFO)
-        end
-      end)
-
-      require('conform').setup {
-        notify_on_error = false,
-        format_on_save = function(bufnr)
-          if vim.b[bufnr].conform_disabled then
-            return nil
-          end
-          return {
-            timeout_ms = 1500,
-            lsp_format = 'fallback',
-          }
-        end,
-        formatters_by_ft = formatters_by_ft,
-        formatters = {
-          stylua = {
-            -- Use nvim stylua config for all lua files
-            prepend_args = { '--config-path', vim.fn.stdpath 'config' .. '/.stylua.toml' },
-          },
-        },
-      }
-    end,
   },
+  config = function()
+    -- TODO: Ensure installed
+
+    ---@type table<string, conform.FiletypeFormatterInternal>
+    local formatters_by_ft = {
+      lua = { 'stylua' },
+      python = { 'ruff_format', 'isort' },
+      sh = { 'shfmt' },
+      -- TODO: Enable this when it stops sucking
+      -- nu = { 'nufmt' },
+    }
+    ---@type table<string, string[]>
+    local general_formatters = {
+      prettierd = { 'javascript', 'typescript', 'json', 'jsonc', 'html', 'javascriptreact', 'typescriptreact' },
+    }
+
+    formatters_by_ft = vim.tbl_extend(
+      'error',
+      formatters_by_ft,
+      vim.iter(pairs(general_formatters)):fold({}, function(acc, fmt, fts)
+        vim.iter(fts):each(function(ft)
+          acc[ft] = { fmt }
+        end)
+        return acc
+      end)
+    )
+
+    vim.keymap.set('n', '<Leader>tf', function()
+      if vim.b.conform_disabled then
+        vim.b.conform_disabled = false
+        vim.notify('[Conform] Enabled', vim.log.levels.INFO)
+      else
+        vim.b.conform_disabled = true
+        vim.notify('[Conform] Disabled', vim.log.levels.INFO)
+      end
+    end)
+
+    require('conform').setup {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        if vim.b[bufnr].conform_disabled then
+          return nil
+        end
+        return {
+          timeout_ms = 1500,
+          lsp_format = 'fallback',
+        }
+      end,
+      formatters_by_ft = formatters_by_ft,
+      formatters = {
+        stylua = {
+          -- Use Neovim .stylua for random lua files
+          -- prepend_args = { '--config-path', vim.fn.stdpath 'config' .. '/.stylua.toml' },
+        },
+      },
+    }
+  end,
 }
