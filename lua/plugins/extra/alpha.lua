2cfcf48d7d80a52b4c77cabab453012bcd893cc0
Matei Stroia <stroiamatei@gmail.com>
2026-05-11 14:34:05 +0300

    add alpha.nvim

---
+++ lua/plugins/extra/alpha.lua
@@ -0,0 +1,26 @@
+--- @type LazyPluginSpec
+return {
+  'goolord/alpha-nvim',
+  enabled = true,
+  config = function()
+    local alpha = require 'alpha'
+    local dashboard = require 'alpha.themes.dashboard'
+
+    local art_path = vim.fs.joinpath(vim.fn.stdpath 'config', 'assets', 'strigoi.txt')
+    dashboard.section.header.val = vim.fn.readfile(art_path)
+
+    local version = vim.version()
+    local version_str = string.format('v%d.%d.%d (%s)', version.major, version.minor, version.patch, version.build)
+    dashboard.section.footer.val = version_str
+
+    dashboard.section.buttons.val = {
+      dashboard.button('e', '  New', ':ene | startinsert <CR>'),
+      dashboard.button('c', '  Config', ':lua require("telescope.builtin").find_files({cwd = vim.env.XDG_CONFIG_HOME})<CR>'),
+      dashboard.button('u', '  Update', ':Lazy update<CR>'),
+      dashboard.button('q', '  Quit', ':q<CR>'),
+      -- TODO: Sessions and MRU
+    }
+
+    alpha.setup(dashboard.config)
+  end,
+}
