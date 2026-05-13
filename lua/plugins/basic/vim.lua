e03472800c6b157b3e2a02d1dd31d6bb422d6ca6
Matei Stroia <stroiamatei@gmail.com>
2026-05-13 12:36:02 +0300

    remove yuck

--- lua/plugins/basic/vim.lua
+++ lua/plugins/basic/vim.lua
@@ -5,17 +5,6 @@ return {
   { src = 'https://github.com/tpope/vim-repeat' },
   { src = 'https://github.com/tridactyl/vim-tridactyl' },
   {
-    src = 'https://github.com/elkowar/yuck.vim',
-    deps = {
-      {
-        src = 'https://github.com/eraserhd/parinfer-rust',
-        build = function(name, path)
-          require('plugins.util').build(name, path, { 'cargo', 'build', '--release' })
-        end,
-      },
-    },
-  },
-  {
     src = 'https://github.com/wakatime/vim-wakatime',
     version = vim.version.range '11.3.0',
   },
