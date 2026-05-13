c71a20c2bae43ace6b1690d8317c71f3b4ad5266
Matei Stroia <stroiamatei@gmail.com>
2026-05-13 16:30:14 +0300

    fix debug 2

--- lua/plugins/lsp/debug.lua
+++ lua/plugins/lsp/debug.lua
@@ -1,14 +1,14 @@
 ---@type LazyPluginSpec
 return {
-  'mfussenegger/nvim-dap',
+  src='https://github.com/mfussenegger/nvim-dap',
   dependencies = {
-    'rcarriga/nvim-dap-ui',
-    'nvim-neotest/nvim-nio',
+    src='https://github.com/rcarriga/nvim-dap-ui',
+    src='https://github.com/nvim-neotest/nvim-nio',
 
-    'mason-org/mason.nvim',
-    'jay-babu/mason-nvim-dap.nvim',
+    src='https://github.com/mason-org/mason.nvim',
+    src='https://github.com/jay-babu/mason-nvim-dap.nvim',
 
-    'mfussenegger/nvim-dap-python',
+    src='https://github.com/mfussenegger/nvim-dap-python',
   },
   keys = {
     {
