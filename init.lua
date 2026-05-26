ac31d7636ea8b0a8b418a3a7ab9a640de3100f79
Matei Stroia <stroiamatei@gmail.com>
2026-05-26 13:27:35 +0300

    opts: fillchars

--- init.lua
+++ init.lua
@@ -22,9 +22,9 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 vim.o.signcolumn = 'yes'
-vim.o.foldcolumn = 'auto:9'
+vim.o.foldcolumn = 'auto'
 vim.o.foldtext = ''
-vim.opt.fillchars = { 'fold: ', 'foldopen:┏', 'foldclose:', 'foldsep:┃' }
+vim.opt.fillchars = { 'fold: ', 'foldopen:┏', 'foldclose:┅', 'foldsep:┃', 'msgsep:━' }
 
 vim.o.updatetime = 500
 vim.o.timeoutlen = 500
